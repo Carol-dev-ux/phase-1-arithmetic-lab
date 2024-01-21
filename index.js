f392302ab955bb4de5f664a273477072eb71f952
Caroline Wangari <karehwachira@gmail.com>
2024-01-22 01:04:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
-const chai = require('chai');
-const expect = chai.expect;
-const { multiply, random, mod, max } = require('../index'); // Assuming index.js is in the parent directory
 
-describe('index.js', function () {
-  describe('multiply', function () {
-    it("is an equation whose multiplied results will equal 62", function () {
-      const num1 = 31;
-      const num2 = 2;
-      expect(num1).to.be.a('number');
-      expect(num2).to.be.a('number');
-      expect(multiply(num1, num2)).to.eq(62);
-    });
-  });
 
-  describe('random', function () {
-    it("generates a random integer greater than 0", function () {
-      const randomValue = random();
-      expect(Number.isInteger(randomValue)).to.be.true;
-      expect(randomValue).to.be.greaterThan(0);
-    });
-  });
+// Implement the multiply function
+function multiply(num1, num2) {
+  return num1 * num2;
+}
 
-  describe('mod', function () {
-    it("is an equation that calculates a remainder that is equal to 4", function () {
-      const num3 = 20;
-      const num4 = 8;
-      expect(num3).to.be.a('number');
-      expect(num4).to.be.a('number');
-      expect(mod(num3, num4)).to.eq(4);
-    });
-  });
+// Implement the random function
+function random() {
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+}
 
-  describe('max', function () {
-    it("will return 20 as the highest number in the set", function () {
-      const result = max(10, 15, 20, 8);
-      expect(result).to.eq(20);
-    });
-  });
-});
+// Implement the mod function
+function mod(num3, num4) {
+  return num3 % num4;
+}
+
+// Implement the max function
+function max(...numbers) {
+  return Math.max(...numbers);
+}
+
+module.exports = { multiply, random, mod, max };
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,30 +1,42 @@
-const chai = require('chai')
-const expect = chai.expect;
+// test/indexTest.js
 
-describe('index.js', function() {
-  describe('multiply', function() {
-    it("is an equation whose multiplied results will equal 62", function() {
-      expect(num1).to.be.a('number')
-      expect(num2).to.be.a('number')
-      expect(multiply).to.eq(62)
+const chai = require('chai');
+const expect = chai.expect;
+const { multiply, random, mod, max } = require('../index');
+
+describe('index.js', function () {
+  describe('multiply', function () {
+    it("is an equation whose multiplied results will equal 62", function () {
+      const num1 = 31;
+      const num2 = 2;
+      expect(num1).to.be.a('number');
+      expect(num2).to.be.a('number');
+      expect(multiply(num1, num2)).to.eq(62);
     });
   });
-  describe('random', function() {
-    it("generates a random integer greater than 0", function() {
-      expect(Number.isInteger(random)).to.be.true
-      expect(random).to.not.eq(0)
+
+  describe('random', function () {
+    it("generates a random integer greater than 0", function () {
+      const randomValue = random();
+      expect(Number.isInteger(randomValue)).to.be.true;
+      expect(randomValue).to.be.greaterThan(0);
     });
   });
-  describe('mod', function() {
-    it("is an equation that calculates a remainder that is equal to 4", function() {
-      expect(num3).to.be.a('number')
-      expect(num4).to.be.a('number')
-      expect(mod).to.eq(4)
+
+  describe('mod', function () {
+    it("is an equation that calculates a remainder that is equal to 4", function () {
+      const num3 = 20;
+      const num4 = 8;
+      expect(num3).to.be.a('number');
+      expect(num4).to.be.a('number');
+      expect(mod(num3, num4)).to.eq(4);
     });
   });
-  describe('max', function() {
-    it("will return 20 as the highest number in the set", function() {
-      expect(max).to.eq(20)
+
+  describe('max', function () {
+    it("will return 20 as the highest number in the set", function () {
+      const result = max(10, 15, 20, 8);
+      expect(result).to.eq(20);
     });
   });
 });
